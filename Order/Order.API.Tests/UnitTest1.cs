a90fa31c634a41b16f7fa4c0c46ad53fa4a16fd6
Chaminda Chandrasekara <chaminda.chandrasekara@fotoware.com>
2023-08-23 13:20:01 +0200

    test fail sample

--- Order/Order.API.Tests/UnitTest1.cs
+++ Order/Order.API.Tests/UnitTest1.cs
@@ -12,5 +12,11 @@ namespace Order.API.Tests
         {
             Assert.Pass();
         }
+
+        [Test]
+        public void Test2()
+        {
+            Assert.Fail();
+        }
     }
 }
